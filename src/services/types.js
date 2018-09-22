377dd6a4275cca058b82d132c59966edfc9cf084
Rodrigo <cherry199662@gmail.com>
2018-09-22 13:08:32 -0500

    reOrder property types

--- src/services/types.js
+++ src/services/types.js
@@ -1,10 +1,21 @@
 import {summary} from './summary.js';
 
 let typesCount = [];
-
-for (let data of summary.property_types) {
-  let {count, id, type: name} = data;
-  typesCount.push({count, name, id});
+let propertyOrderList = {
+  '3' : "Casa",
+  '2' : "Departamento",
+  '1' : "Terreno",
+  '7' : "Local",
+  '5' : "Oficina",
 }
+Object.keys(propertyOrderList).map((order)=>{
+  summary.property_types.map((data) => {
+    let {count, id, type: name} = data;
+    if(order == id){
+      typesCount.push({count, name, id});
+    }
+  })
+})
+
 
 export let types = typesCount;
